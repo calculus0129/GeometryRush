1b7b7aa332c94f3b8eda211850b676cac1c9a8ea
jaehyun.bhang <ryanbhang129@naver.com>
2025-08-01 15:49:10 +0900

    style: :sparkles: add type to canvas

--- client/src/game/GameRenderer.ts
+++ client/src/game/GameRenderer.ts
@@ -6,7 +6,7 @@ import { Spike } from "./Spike";
 export class GameRenderer {
   ctx: CanvasRenderingContext2D;
   camera: Camera;
-  canvas: any;
+  canvas: HTMLCanvasElement;
   constructor(ctx: CanvasRenderingContext2D, camera: Camera) {
     this.ctx = ctx;
     this.camera = camera;
